cb9b36adcfb20dd82094aa15406ce19c40a78d03
Artem <slivnitsin.art@mail.ru>
2022-08-27 04:04:40 +0800

    task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int arg = Convert.ToInt32(Console.ReadLine());
+            
+Console.WriteLine(arg == int.Parse(new string(arg.ToString().Reverse().ToArray())));
